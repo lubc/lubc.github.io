0c93c57269cd0bf09147100d361170256efcc3a5
Luciano <luchob.32@gmail.com>
2016-03-07 02:43:18 -0300

    new page

---
+++ javascripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var rotate = document.getElementById( 'rotate' );
+
+  if (rotate) {
+    rotate.addEventListener( 'mouseover', function () {
+        
+        this.className = 'over';
+        
+    }, false );
+
+    rotate.addEventListener( 'mouseout', function () {
+        
+        var rotate = this;
+        
+        rotate.className = 'out';
+        window.setTimeout( function () { rotate.className = '' }, 550 );
+        
+    }, false );
+  }
+});
