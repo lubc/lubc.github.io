f825dfab2a5539ca783deb7b842fe6c11fb6564e
Luciano <luchob.32@gmail.com>
2017-11-20 17:56:52 -0300

    Update assets

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var rotate = document.getElementById( 'rotate' );
+document.addEventListener("DOMContentLoaded", function(event) {
+  var rotate = document.getElementById('rotate');
 
-  if (rotate) {
-    rotate.addEventListener( 'mouseover', function () {
-        
-        this.className = 'over';
-        
-    }, false );
+  if(rotate) {
+    rotate.addEventListener('mouseover', function() {
+      this.className = 'over';
+    }, false);
 
-    rotate.addEventListener( 'mouseout', function () {
-        
-        var rotate = this;
-        
-        rotate.className = 'out';
-        window.setTimeout( function () { rotate.className = '' }, 550 );
-        
-    }, false );
+    rotate.addEventListener('mouseout', function() {
+      var rotate = this;
+      rotate.className = 'out';
+      window.setTimeout(function() { rotate.className = '' }, 550);
+    }, false);
   }
 });
