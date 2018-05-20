1bdc71f3b172697a542cdeb9a462a6da8876f038
Luciano Becerra <luchob.32@gmail.com>
2018-05-19 23:01:44 -0300

    JS style

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var rotate = document.getElementById('rotate');
+  var rotate = document.getElementById("rotate");
 
-  if(rotate) {
-    rotate.addEventListener('mouseover', function() {
-      this.className = 'over';
-    }, false);
+  if (rotate) {
+    rotate.addEventListener(
+      "mouseover",
+      function() {
+        this.className = "over";
+      },
+      false
+    );
 
-    rotate.addEventListener('mouseout', function() {
-      var rotate = this;
-      rotate.className = 'out';
-      window.setTimeout(function() { rotate.className = '' }, 550);
-    }, false);
+    rotate.addEventListener(
+      "mouseout",
+      function() {
+        var rotate = this;
+        rotate.className = "out";
+        window.setTimeout(function() {
+          rotate.className = "";
+        }, 550);
+      },
+      false
+    );
   }
 });
